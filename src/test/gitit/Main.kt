17f0569fe7c0f9781c5147a1232e24b021d8a1fb
Dejan Dobnikar <dejan.dobnikar@gmail.com>
2018-04-01 21:22:02 +0200

    bear, param check

--- src/test/gitit/Main.kt
+++ src/test/gitit/Main.kt
@@ -13,6 +13,7 @@ class Main {
             letterPrinter("X", 6)
             shrugPrint()
             flipTable()
+            bear(3)
         }
 
         fun starPrinter(count: Int) {
@@ -63,6 +64,20 @@ class Main {
             System.out.println("flipping table...")
             //language=TEXT
             System.out.println("(╯°□°）╯︵ ┻━┻")
+        }
+
+        fun bear(count: Int) {
+
+            if (count < 0) {
+                throw IllegalStateException()
+            }
+
+            System.out.println()
+            //language=TEXT
+            (1..count).forEach( {
+                System.out.print("ʕ•ᴥ•ʔ ")
+            })
+            System.out.println()
         }
     }
 
